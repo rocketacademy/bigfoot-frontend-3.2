adb9ddc1e1e7da77fc83835a0ebea1f2d631cf2f
estney <esther.pxy@gmail.com>
2024-01-15 16:17:46 +0800

    all sightings displayed on site

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,62 @@
-import logo from "/logo.png";
-
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { backendURL } from "./db";
 
 function App() {
+  const [BFSightings, setBFSightings] = useState([]);
+
+  // pull data from localhost 3000
+  // use async and await
+  useEffect(() => {
+    const bigfootdata = async () => {
+      try {
+        const response = await axios.get(`${backendURL}/sightings/`);
+        const bigFootData = response.data;
+        // store data in states
+        setBFSightings(bigFootData);
+        console.log(`bigfoot sightings`, bigFootData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    bigfootdata();
+  }, []);
+
+  //display all sightings
+  const SightingsList = ({ sightings }) => (
+    <ul>
+      {sightings.map((sighting, index) => (
+        <li key={index}>
+          <strong>Sighting {index + 1}:</strong>
+          <ul>
+            <li>
+              <strong>Date: </strong> {sighting.DATE} {sighting.MONTH},{" "}
+              {sighting.YEAR}
+            </li>
+            <li>
+              <strong>Season:</strong> {sighting.SEASON}
+            </li>
+            <li>
+              <strong>Location:</strong> {sighting.LOCATION_DETAILS},{" "}
+              {sighting.STATE}
+            </li>
+            <li>
+              <strong>Observation:</strong> {sighting.OBSERVED}
+            </li>
+            <br />
+          </ul>
+        </li>
+      ))}
+    </ul>
+  );
+
   return (
     <>
-      <div>
-        <img src={logo} className="logo react" alt="React logo" />
-      </div>
       <h1>Bigfoot Frontend </h1>
       <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h3>Bigfoot Sightings</h3>
+        <SightingsList sightings={BFSightings} />
       </div>
     </>
   );
