33cd1f46ece4f54e38d316020804f570065a5944
Chxshire22 <akuindra22@gmail.com>
2024-01-16 02:13:00 +0800

    installed react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import logo from "/logo.png";
 import axios from "axios"
 import "./App.css";
 import { useEffect, useState } from "react";
+import {RouterProvider, createBrowserRouter} from "react-router-domnpm "
 
 
 function App() {
 
-  const [sightings, setSightings] = useState({})
+  const [sightings, setSightings] = useState([])
 
   useEffect(()=>{
     axios.get("http://localhost:3001/sightings")
@@ -17,11 +18,29 @@ function App() {
       .catch(err=>console.log(err))
   },[])
 
-  // const SightingArr = sightings.map((sightings.data)=>(
-  //   <div>
-  //     <p>{sighting.data}</p>
-  //   </div>
-  // ))
+
+
+  const sightingArr = sightings.map((sighting, index) => (
+    <li key={index} className="report-list__item">
+      <div className="list__item-container">
+        <div className="container__report-detail">
+          <p className="report-detail">{sighting.REPORT_NUMBER}</p>
+        </div>
+        <div className="container__report-detail">
+          <p className="report-detail">{sighting.STATE}</p>
+        </div>
+        <div className="container__report-detail">
+          <p className="report-detail">{sighting.COUNTY}</p>
+        </div>
+        <div className="container__report-detail">
+          <p className="report-detail">{sighting.YEAR}</p>
+        </div>
+        <div className="container__report-detail">
+          <p className="report-detail">{sighting.REPORT_CLASS}</p>
+        </div>
+      </div>
+    </li>
+  ));
 
   return (
     <>
@@ -29,11 +48,32 @@ function App() {
         <img src={logo} className="logo react" alt="React logo" />
       </div>
       <h1>Bigfoot Frontend </h1>
+      <p>
+        <small>You know what they say about big feet...</small>
+      </p>
       <div className="card">
-        <p>{sightings[0]?.YEAR}</p>
-        <p>
-          <small>You know what they say about big feet...</small>
-        </p>
+        <ol className="report-list">
+          <div className="report-list__header">
+            <div className="list__item-container">
+              <div className="container__report-detail">
+                <p className="report-detail">REPORT NUMBER</p>
+              </div>
+              <div className="container__report-detail">
+                <p className="report-detail">STATE</p>
+              </div>
+              <div className="container__report-detail">
+                <p className="report-detail">COUNTY</p>
+              </div>
+              <div className="container__report-detail">
+                <p className="report-detail">YEAR</p>
+              </div>
+              <div className="container__report-detail">
+                <p className="report-detail">REPORT CLASS</p>
+              </div>
+            </div>
+          </div>
+          {sightingArr}
+        </ol>
       </div>
     </>
   );
