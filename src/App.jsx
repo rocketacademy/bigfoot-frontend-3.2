a3d7363b777902b918d962b66198a52a1cc04867
karenhnlai <karenhnlai@gmail.com>
2024-02-05 14:48:10 +0800

    base almost completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import logo from "/logo.png";
-
 import "./App.css";
+import SightingsList from "./Components/SightingsList";
+// import SightingsDetail from "./Components/SightingsDetail";
 
 function App() {
   return (
     <>
       <div>
-        <img src={logo} className="logo react" alt="React logo" />
+        <img src={logo} className="logo react" alt="logo" />
       </div>
-      <h1>Bigfoot Frontend </h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Bigfoot Sightings</h1>
+      <p>Click a ID number to get more information on that bigfoot sighting.</p>
+      <SightingsList />
+      {/* <SightingsDetail /> */}
     </>
   );
 }
